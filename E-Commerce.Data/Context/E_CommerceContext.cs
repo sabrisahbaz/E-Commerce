ee5f82af5ca76ad88ecea5035709fbbb23d6cbf0
KİNG OF GAMER <sabrisahbaz1905@gmail.com>
2024-04-17 20:05:47 +0300

    Adding Nuget Package (SqlServer,Tools,Identity) Entitites were created, database connection was made and migration was started

---
+++ E-Commerce.Data/Context/E_CommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Data.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Data.Context
+{
+    public class E_CommerceContext :
+            IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;Database=ECommerceDb;User Id=sa;Password=1234;TrustServerCertification=true;");
+        }
+    }
+}
